3a84ab8322d96a01607feafed31ca15da2dbdcaa
Arhaan25 <101710365+Arhaan25@users.noreply.github.com>
2023-01-30 19:26:10 +0530

    Add files via upload

---
+++ Bedroom_Set.js
@@ -0,0 +1,52 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage('Bedroom Set.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function back() {
+    window.location = "index.html";
+}
+
+function draw() {
+    image(img, 0, 0, 500, 500);
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for (i=0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            fill(r, g, b);
+            noFill();
+            stroke(r, g, b);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,23 @@
+function Bedroom_Set() {
+    window.location = "Bedroom_Set.html";
+}
+
+function Dining_Set() {
+    window.location = "Dining_Set.html";
+}
+
+function Plate_Set() {
+    window.location = "Plate_Set.html";
+}
+
+function Pokemon() {
+    window.location = "Pokemon.html";
+}
+
+function Sports_Equipment() {
+    window.location = "Sports_Equipment.html";
+}
+
+function Stationary_Set() {
+    window.location = "Stationary_Set.html";
+}
